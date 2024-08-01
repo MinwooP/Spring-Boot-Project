d9cf944dad2b7d1bc99a8f71d96eb96ef7a2779e
박민우 <alsd20177@gmail.com>
2024-08-01 15:28:17 +0900

    [#2] - 회원 도메인과 레포지토리 테스트

--- src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hello_spring/repository/MemoryMemberRepository.java
@@ -10,28 +10,33 @@ public class MemoryMemberRepository implements MemberRepository{
 
     @Override
     public Member save(Member member) {
+        // id에 해당하는 멤버를 등록
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
     }
 
     @Override
+    // id 에 해당하는 member를 탐색 및 반환
     public Optional<Member> findById(Long id) {
         return Optional.ofNullable(store.get(id));
     }
 
     @Override
+    // 모든 member를 반환
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
     }
 
     @Override
+    // 특정 name에 해당하는 member를 반환
     public Optional<Member> findByName(String name) {
         return store.values().stream()
                 .filter(member -> member.getName().equals(name))
                 .findAny();
     }
 
+    // 모든 member를 삭제
     public void clearStore() {
         store.clear();
     }
